1b722610d1067e0c3fabc35add897e15319b2880
hosadone <saikalekar627@gmail.com>
2023-11-17 23:49:12 -0800

    hoss

---
+++ Mongo/NewmongodbApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewmongodbApplication.class, args);
+	}
+}
